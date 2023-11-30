f8b46d2ea5c72010e3085f8dcccaecb94dccd63a
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2023-11-30 13:16:36 +0000

    safe-local-variables

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "f91395598d4cb3e2ae6a2db8527ceb83fed79dbaf007f435de3e91e5bda485fb" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
- )
+ '(safe-local-variable-values '((lsp-disabled-clients elixir-ls))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
