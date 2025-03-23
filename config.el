db58ecd7ba2fcae9e1035a54ae3c6ede9441dff1
mccraigmccraig <mccraigmccraig@gmail.com>
2025-03-23 09:04:45 +0000

    better treesitter setup
    - including auto-config and loading of grammars
    - and mode overrides

--- config.el
+++ config.el
@@ -258,40 +258,118 @@
   :map flycheck-command-map
   "l" #'consult-flycheck))
 
-(with-eval-after-load 'eglot
-  (setf (alist-get '(elixir-mode elixir-ts-mode heex-ts-mode)
-                   eglot-server-programs
-                   nil nil #'equal)
-        (if (and (fboundp 'w32-shell-dos-semantics)
-                 (w32-shell-dos-semantics))
-            '("language_server.bat")
-          (eglot-alternatives
-           '("/Users/mccraigmccraig/bin/lexical/_build/dev/package/lexical/bin/start_lexical.sh")))))
+;; (with-eval-after-load 'eglot
+;;   (setf (alist-get '(elixir-mode elixir-ts-mode heex-ts-mode)
+;;                    eglot-server-programs
+;;                    nil nil #'equal)
+;;         (if (and (fboundp 'w32-shell-dos-semantics)
+;;                  (w32-shell-dos-semantics))
+;;             '("language_server.bat")
+;;           (eglot-alternatives
+;;            '("/Users/mccraigmccraig/bin/lexical/_build/dev/package/lexical/bin/start_lexical.sh")))))
 
-;; this doesn't set up the auto-mode-alist mappings for
-;; elixir unfortunately - the elixir-mode stuff clashes
-;; and overrides...
-(use-package! elixir-ts-mode
-  :mode (("\\.heex\\'" . heex-ts-mode)
-         ("\\.ex\\'" . elixir-ts-mode))
+;; ;; this doesn't set up the auto-mode-alist mappings for
+;; ;; elixir unfortunately - the elixir-mode stuff clashes
+;; ;; and overrides...
+;; (use-package! elixir-ts-mode
+;;   :mode (("\\.heex\\'" . heex-ts-mode)
+;;          ("\\.ex\\'" . elixir-ts-mode))
 
+;;   :config
+;;   (add-hook! '(elixir-ts-mode-hook) #'lsp!))
+
+;; treesit
+
+(use-package treesit
+  :mode (("\\.tsx\\'" . tsx-ts-mode)
+         ("\\.js\\'"  . typescript-ts-mode)
+         ("\\.mjs\\'" . typescript-ts-mode)
+         ("\\.mts\\'" . typescript-ts-mode)
+         ("\\.cjs\\'" . typescript-ts-mode)
+         ("\\.ts\\'"  . typescript-ts-mode)
+         ("\\.jsx\\'" . tsx-ts-mode)
+         ("\\.json\\'" .  json-ts-mode)
+         ("\\.Dockerfile\\'" . dockerfile-ts-mode)
+         ("\\.prisma\\'" . prisma-ts-mode)
+         ("\\.ex\\'" . elixir-ts-mode)
+         ("\\.exs\\'" . elixir-ts-mode)
+         ("\\.heex\\'" . heex-ts-mode)
+         ;; More modes defined here...
+         )
+  :preface
+  (defun os/setup-install-grammars ()
+    "Install Tree-sitter grammars if they are absent."
+    (interactive)
+    (dolist (grammar
+             '((css . ("https://github.com/tree-sitter/tree-sitter-css" "v0.20.0"))
+               (bash "https://github.com/tree-sitter/tree-sitter-bash")
+               (html . ("https://github.com/tree-sitter/tree-sitter-html" "v0.20.1"))
+               (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript" "v0.21.2" "src"))
+               (json . ("https://github.com/tree-sitter/tree-sitter-json" "v0.20.2"))
+               (python . ("https://github.com/tree-sitter/tree-sitter-python" "v0.20.4"))
+               (go "https://github.com/tree-sitter/tree-sitter-go" "v0.20.0")
+               (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+               (make "https://github.com/alemuller/tree-sitter-make")
+               (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+               (cmake "https://github.com/uyha/tree-sitter-cmake")
+               (c "https://github.com/tree-sitter/tree-sitter-c")
+               (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+               (toml "https://github.com/tree-sitter/tree-sitter-toml")
+               (tsx . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "tsx/src"))
+               (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript" "v0.20.3" "typescript/src"))
+               (yaml . ("https://github.com/ikatyang/tree-sitter-yaml" "v0.5.0"))
+               (prisma "https://github.com/victorhqc/tree-sitter-prisma")
+               (elixir . ("https://github.com/elixir-lang/tree-sitter-elixir" "v0.3.4" "src"))
+               (heex . ("https://github.com/phoenixframework/tree-sitter-heex" "v0.8.0" "src"))))
+      (add-to-list 'treesit-language-source-alist grammar)
+      ;; Only install `grammar' if we don't already have it
+      ;; installed. However, if you want to *update* a grammar then
+      ;; this obviously prevents that from happening.
+      (unless (treesit-language-available-p (car grammar))
+        (treesit-install-language-grammar (car grammar)))))
+
+  ;; Optional, but recommended. Tree-sitter enabled major modes are
+  ;; distinct from their ordinary counterparts.
+  ;;
+  ;; You can remap major modes with `major-mode-remap-alist'. Note
+  ;; that this does *not* extend to hooks! Make sure you migrate them
+  ;; also
+  (dolist (mapping
+           '((python-mode . python-ts-mode)
+             (css-mode . css-ts-mode)
+             (typescript-mode . typescript-ts-mode)
+             (js-mode . typescript-ts-mode)
+             (js2-mode . typescript-ts-mode)
+             (c-mode . c-ts-mode)
+             (c++-mode . c++-ts-mode)
+             (c-or-c++-mode . c-or-c++-ts-mode)
+             (bash-mode . bash-ts-mode)
+             (css-mode . css-ts-mode)
+             (json-mode . json-ts-mode)
+             (js-json-mode . json-ts-mode)
+             (sh-mode . bash-ts-mode)
+             (sh-base-mode . bash-ts-mode)
+             (elixir-mode . elixir-ts-mode)
+             ))
+    (add-to-list 'major-mode-remap-alist mapping))
   :config
-  (add-hook! '(elixir-ts-mode-hook) #'lsp!))
+  (os/setup-install-grammars))
+
 
 ;; aidermacs
 
-(after! epa
+;; (after! epa
 
-  (load-library "/Users/mccraigmccraig/.doom.d/secrets.el.gpg")
+;;   (load-library "/Users/mccraigmccraig/.doom.d/secrets.el.gpg")
 
-  (use-package! aidermacs
-    :bind (("C-c a" . aidermacs-transient-menu))
-    :config
-                                        ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
-    (setenv "ANTHROPIC_API_KEY" mccraigmccraig-anthropic-api-key)
-                                        ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
-    (setenv "OPENAI_API_KEY" mccraigmccraig-openai-api-key)
-    :custom
-                                        ; See the Configuration section below
-    ;; (aidermacs-use-architect-mode t)
-    (aidermacs-default-model "sonnet")))
+;;   (use-package! aidermacs
+;;     :bind (("C-c a" . aidermacs-transient-menu))
+;;     :config
+;;                                         ; Set API_KEY in .bashrc, that will automatically picked up by aider or in elisp
+;;     (setenv "ANTHROPIC_API_KEY" mccraigmccraig-anthropic-api-key)
+;;                                         ; defun my-get-openrouter-api-key yourself elsewhere for security reasons
+;;     (setenv "OPENAI_API_KEY" mccraigmccraig-openai-api-key)
+;;     :custom
+;;                                         ; See the Configuration section below
+;;     ;; (aidermacs-use-architect-mode t)
+;;     (aidermacs-default-model "sonnet")))
