ecc0c5acad8e3569cb1aaf1c5084cfc773448101
mccraigmccraig <mccraigmccraig@gmail.com>
2025-03-24 12:33:46 +0000

    add combobulate

--- config.el
+++ config.el
@@ -353,7 +353,22 @@
              ))
     (add-to-list 'major-mode-remap-alist mapping))
   :config
-  (os/setup-install-grammars))
+  (os/setup-install-grammars)
+
+
+  ;; Do not forget to customize Combobulate to your liking:
+  ;;
+  ;;  M-x customize-group RET combobulate RET
+  ;;
+  (use-package combobulate
+    :custom
+    ;; You can customize Combobulate's key prefix here.
+    ;; Note that you may have to restart Emacs for this to take effect!
+    (combobulate-key-prefix "C-c o")
+    :hook ((prog-mode . combobulate-mode))
+    ;; Amend this to the directory where you keep Combobulate's source
+    ;; code.
+    :load-path ("/Users/mccraigmccraig/lib/combobulate")))
 
  ;;;; Code Completion
 (use-package corfu
