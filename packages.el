67e6f4ca3fecc741312608380793c83ae55b2b87
mccraigmccraig <mccraigmccraig@gmail.com>
2025-10-16 11:20:58 +0100

    eat config

--- packages.el
+++ packages.el
@@ -67,3 +67,13 @@
            :files ("*.el" (:exclude "images/*"))))
 
 (package! windresize)
+
+(package! eat
+  :recipe (:type git
+           :host codeberg
+           :repo "akib/emacs-eat"
+           :files ("*.el" ("term" "term/*.el") "*.texi"
+                   "*.ti" ("terminfo/e" "terminfo/e/*")
+                   ("terminfo/65" "terminfo/65/*")
+                   ("integration" "integration/*")
+                   (:exclude ".dir-locals.el" "*-tests.el"))))
