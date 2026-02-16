3d765e075497987027bae964e672aa3f0ee4a1d7
mccraigmccraig <mccraigmccraig@gmail.com>
2026-02-16 12:12:57 +0000

    org-tree-slide

--- packages.el
+++ packages.el
@@ -77,3 +77,5 @@
                    ("terminfo/65" "terminfo/65/*")
                    ("integration" "integration/*")
                    (:exclude ".dir-locals.el" "*-tests.el"))))
+
+(package! org-tree-slide)
